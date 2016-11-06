e9fd4c8631fbb2d58b02e4bade85ddbd64145e1e
Darragh King <mail@darraghking.com>
2016-11-06 00:31:11 +0000

    Clicking route links in sidebar now marks stops from that route on map. Added clear map functionality, and zoom functionality (cannot interact with map when not on mobile, need to address this). NB.. This app only works on HTTPS for mobile.

--- js/map.js
+++ js/map.js
@@ -1,13 +1,16 @@
 /**
  * Created by Darragh on 04/11/2016.
  */
-var crd, clat, clong, currentLocation;
+var crd, clat, clong, currentLocation, map, here, zoom;
+var stop = 'images/stop.png';
+var markers = [];
 
 
 
 //getting current location data from browser
-navigator.geolocation.getCurrentPosition(function success(pos)
-{
+navigator.geolocation.getCurrentPosition(success, error);
+
+function success(pos) {
     //calculate location
     crd = pos.coords;
     clat = crd.latitude;
@@ -17,11 +20,18 @@ navigator.geolocation.getCurrentPosition(function success(pos)
     console.log('Latitude : ' + clat);
     console.log('Longitude: ' + clong);
     console.log('Accurate to approx. ' + crd.accuracy + ' meters.');
+    zoom = 17;
     //setup map
-    initMap();
-});
+    drawMap();
+}
 
-function initMap() {
+function error() {
+    alert('Oops!');
+}
+
+function drawMap()
+{
+    console.log('Drawing Map');
     //get current location LatLang
     currentLocation = new google.maps.LatLng(clat, clong);
     //style the map
@@ -57,22 +67,20 @@ function initMap() {
     //set map options
     options =
     {
-        zoom: 18,
+        zoom: zoom,
         center: currentLocation,
         disableDefaultUI: true,
-        mapTypeId: 'hybrid',
-        styles: style
+        mapTypeId: 'hybrid'
     };
     //create new map with previous requirements/specs
-    var map = new google.maps.Map(document.getElementById("map"), options);
+    map = new google.maps.Map(document.getElementById("map"), options);
     //mark location on map with custom marker
     markCurrentLocation(map);
 }
 
-function markCurrentLocation(map)
-{
+function markCurrentLocation(map) {
     //define marker image
-    var here = '/images/here.png';
+    here = '/images/here.png';
     //define marker position
     var marker = new google.maps.Marker({
         //set position(current location variable passed here) & icon
@@ -83,4 +91,58 @@ function markCurrentLocation(map)
     });
     //place marker
     marker.setMap(map);
+}
+
+//pass in lat and long of stop
+function markStop(lat, lon) {
+    var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(lat, lon),
+            map: map,
+            animation: google.maps.Animation.DROP,
+            icon: stop
+    });
+    //add marker to markers array
+    markers.push(marker);
+}
+
+function getRoute(routeNumber) {
+    var current = new google.maps.LatLng(clat, clong);
+    //clear the screen before drawing route
+    clearMarkers();
+    //add markers to map
+    drawMarkers(map);
+
+    //get stop locations and draw the stops
+    $.getJSON('json/'+routeNumber+'.json', function (data) {
+        //mark all the outbound stops
+        data.outbound.forEach(function(outbound) {
+            markStop(outbound.latitude, outbound.longitude);
+        });
+        //mark all the inbound stops
+        data.inbound.forEach(function(inbound) {
+            markStop(inbound.latitude, inbound.longitude)
+        });
+    });
+    console.log(markers);
+    //draw stops on map
+    drawMarkers(map);
+}
+
+function drawMarkers(routemap){
+    for(var i = 0; i<markers.length; i++)
+        markers[i].setMap(routemap);
+}
+
+function clearMarkers() {
+    for(var i=0; i < this.markers.length; i++){
+        this.markers[i].setMap(null);
+    }
+    this.markers = new Array();
+};
+
+function setZoom(direction){
+    //if direction is true zoom in, otherwise zooom out
+    direction ? zoom++ : zoom--;
+    drawMap();
+    drawMarkers(map);
 }
